b9483531d2b9e13da5b8fc67404bfdf6fd25bb0a
Igor Entaltsev <igor.yentaltsev@gmail.com>
2013-07-17 11:42:19 -0400

    bug877269_i - added bones

--- test/test.js
+++ test/test.js
@@ -1,202 +1,543 @@
 var assert = require( 'assert' ),
-    fork = require( 'child_process' ).fork,
-    request = require( 'request' ),
-    MakeAPI = require( '../public/js/make-api.js' ),
-    now = Date.now(),
-    child,
-    loginChild,
-    port = 3000,
-    hostAuth = 'http://travis:travis@localhost:' + port,
-    hostNoAuth = 'http://localhost:' + port,
-    // One admin user, one non-admin
-    admin = {
-      email: "admin@webfaker.org",
-      username: "admin",
-      fullName: "An Admin",
-      isAdmin: true },
-    notAdmin = {
-      email: "notadmin@webfaker.org",
-      username: "notadmin",
-      fullName: "Not Admin",
-      isAdmin: false
-    };
+  fork = require( 'child_process' ).fork,
+  request = require( 'request' ),
+  MakeAPI = require( '../public/js/make-api.js' ),
+  now = Date.now(),
+  child,
+  loginChild,
+  port = 3000,
+  hostAuth = 'http://travis:travis@localhost:' + port,     
+  hostNoAuth = 'http://localhost:' + port,
+  illegalChars = "` ! @ # $ % ^ & * ( ) + = ; : ' \" , < . > / ?".split( " " ); console.log('11 hostNoAuth >>> '+ hostNoAuth);
+  
+      // One admin user, one non-admin
+      admin = {
+        email: "admin@webfaker.org",
+        username: "admin",
+        fullName: "An Admin",
+        isAdmin: true },
+        notAdmin = {
+          email: "notadmin@webfaker.org",
+          username: "notadmin",
+          fullName: "Not Admin",
+          isAdmin: false,
+        };                                                          console.log('24 admin >>> '+ admin);
 
-/**
- * Server functions
- */
-function startLoginServer( done ) {
-  var loginPort = 3001;
 
-  function createUser( user, callback ) {
-    request({
-      url: "http://localhost:" + loginPort + "/user",
-      method: 'post',
-      json: user
-    }, function( err, res, body ) {
-      assert.ok( !err );
-      assert.equal( res.statusCode, 200 );
-      callback();
-    });
-  }
+      // Adding a space to the illegalChars list
+      illegalChars.push(" ");
 
-  loginChild = fork( 'test/login.webmaker.org/app.js', [], { env: {
-    PORT: loginPort,
-    HOSTNAME: "http://localhost",
-    MONGO_URL: "mongodb://localhost:27017/local_webmakers",
-    SESSION_SECRET: "secret",
-    ALLOWED_USERS: "travis:travis",
-    ALLOWED_DOMAINS: "http://localhost:3000 http://localhost:3001"
-  }});
-
-  loginChild.on( "message", function( msg ) {
-    if( msg === "Started" ) {
-      // Create a few logins
-      createUser( admin, function() {
-        createUser( notAdmin, done );
-      });
+  /**
+   * Server functions
+   */
+   function startLoginServer( done ) {   console.log('33 in startLoginServer()');
+    var loginPort = 3001;                console.log('34 loginPort >>> '+ loginPort);
+                                         console.log('35 before createUser()');
+    function createUser( user, callback ) {   console.log('36 in createUser()'); console.log('user >>> '+ user); console.log('callback >>> '+ callback);
+      request({
+        url: "http://localhost:" + loginPort + "/user",
+        method: 'post',
+        json: user
+      }, function( err, res, body ) {                   console.log('41 in createUser()');
+        assert.ok( !err );
+        assert.equal( res.statusCode, 200 );
+        callback();
+      }); console.log('url >>> '+ url);
     }
-  });
-}
-
-function startServer( done ) {
-  startLoginServer( function() {
-    // Spin-up the MakeAPI server as a child process
-    child = fork( 'server.js', null, {} );
-    child.on( 'message', function( msg ) {
-      if ( msg === 'Started' ) {
-        done();
+                                                        if (request) console.log('47 after create user.');
+    loginChild = fork( 'login.webmaker.org/app.js', [], { env: {
+      PORT: loginPort,
+      HOSTNAME: "http://localhost",
+      MONGO_URL: "mongodb://localhost:27017/local_webmakers",
+      SESSION_SECRET: "secret",
+      ALLOWED_USERS: "travis:travis",
+      ALLOWED_DOMAINS: "http://localhost:3000 http://localhost:3001"
+    }});
+                                                         console.log('56 loginChild >>> '+ loginChild); 
+    loginChild.on( "message", function( msg ) {          console.log('57 msg >>> '+ msg);
+      if( msg === "Started" ) {
+        // Create a few logins
+        createUser( admin, function() {
+          createUser( notAdmin, done );
+        });
       }
     });
-  });
-}
-
-function stopServer( done ) {
-  loginChild.kill();
-  child.kill();
-  done();
-}
-
-/**
- * Api functions
- */
-
-function apiHelper( verb, uri, httpCode, data, callback, assertions ) {
-  // Parameter handling
-  if ( typeof( data ) === "function" ) {
-    callback = data;
-    data = {};
-  } else {
-    data = data || {};
   }
-  callback = callback || function(){};
-  assertions = assertions || function ( err, res, body, callback ) {
-    assert.ok( !err );
-    assert.equal( res.statusCode, httpCode );
-    callback( err, res, body );
-  };
+  
+  function startServer( done ) {               console.log('67 in startServer()');
+    startLoginServer( function() {             //console.log('in startLoginServer');   
+      // Spin-up the MakeAPI server as a child process
+      child = fork( 'server.js', null, {} );   //console.log('child >>> '+ child);
+      child.on( 'message', function( msg ) {
+        if ( msg === 'Started' ) {
+          done();
+        }
+      });
+    });
+  }
+  
+  function stopServer( done ) {
+    loginChild.kill();
+    child.kill();
+    done();
+  }
+  
+  /**
+   * Api functions
+   */
 
-  request({
-    url: uri,
-    method: verb,
-    json: data
-  }, function( err, res, body ) {
-    assertions( err, res, body, callback );
-  });
-}
-
-/**
- * User functions
- */
-
-function unique( options ) {
-  options = options || {};
-  var u = ( ++now ).toString( 36 ),
-      user = options.user || admin,
-      url = 'http://' + user.username + '.makes.org/' + u;
-
-  return {
-    maker: user.email,
-    make: {
-      url: url,
-      locale: options.locale || "en_US",
-      contentType: options.contentType || 'text/html',
-      title: options.title || u,
-      description: options.description || u,
-      thumbnail: options.thumbnail || url + "/thumbnail.png",
-      author: options.author || user.fullName,
-      email: options.email || user.email,
-      tags: options.tags,
-      remixedFrom: options.remixedFrom
+   function apiHelper( verb, uri, httpCode, data, callback, assertions ) {    console.log('89 in apiHelper()');
+    // Parameter handling
+    if ( typeof( data ) === "function" ) {
+      callback = data;
+      data = {};
+    } else {
+      data = data || {};
     }
-  };
-}
+    callback = callback || function(){};
+    assertions = assertions || function ( err, res, body, callback ) {
+      assert.ok( !err );
+      assert.equal( res.statusCode, httpCode );
+      callback( err, res, body );
+    };
 
-/**
- * Unit tests
- */
+    request({
+      url: uri,
+      method: verb,
+      json: data
+    }, function( err, res, body ) {
+      assertions( err, res, body, callback );
+    });
+  }
+  
+  /**
+   * User functions
+   */
 
-describe( 'POST /make (create)', function() {
+   function unique( options ) {           console.log('117 in unique()');
+    options = options || {};
+    var u = ( ++now ).toString( 36 ),
+    user = options.user || admin,
+    url = 'http://' + user.username + '.makes.org/' + u;
 
-  var api = hostAuth + '/api/make';
+    return {
+      maker: user.email,
+      make: {
+        url: url,
+        locale: options.locale || "en_US",
+        contentType: options.contentType || 'text/html',
+        title: options.title || u,
+        description: options.description || u,
+        thumbnail: options.thumbnail || url + "/thumbnail.png",
+        author: options.author || user.fullName,
+        email: options.email || user.email,
+        tags: options.tags,
+        remixedFrom: options.remixedFrom
+      }
+    };
+  }
+  
+  /**
+   * Unit tests
+   */
+   // A
+  describe( 'POST /api/make (create)', function() {
+                                          console.log('145 in describe A');
+    var api = hostAuth + '/api/make';     console.log('146 api >>> '+ api);
 
-  before( function( done ) {
-    startServer( done );
-  });
+    before( function( done ) {            console.log('148 in before()');
+      startServer( done );      
+    });
 
-  after( function( done ) {
-    stopServer( done );
-  });
+    after( function( done ) {
+      stopServer( done );                 console.log('153 in after()');
+    });  
+    // A1
+    it( 'should create a new make', function( done ) {
+      var m = unique(); console.log('in A1');
 
-  it( 'should create a new make', function( done ) {
-    var m = unique();
+      apiHelper( 'post', api, 200, m, function( err, res, body ) {
+        // Simple test, needs to be expanded for other properties we expect
+        assert.equal( body.url, m.make.url );
+        done();
+      });
+    });
 
-    apiHelper( 'post', api, 200, m, function( err, res, body ) {
-      // Simple test, needs to be expanded for other properties we expect
-      assert.equal( body.url, m.make.url );
+    // A2
+    it( 'make-api.js - id', function( done ) {
+      var m = unique();
+
+      apiHelper( 'post', api, 200, m, function( err, res, body ) {
+        var make = MakeAPI({ apiURL: hostNoAuth, auth: "travis:travis" });
+
+        console.log("body", body);
+
+        make.id( body.id ).then( function( err, data ) {
+
+          console.log("data", data);
+
+          assert.ok( !err );
+          assert.ok( !!data );
+          assert.equal( data[ 0 ].id, body.id );
+          done();
+        });
+      });
+    });
+
+    // A3
+    it( 'make-api.js - url', function( done ) {
+      var m = unique();
+
+      apiHelper( 'post', api, 200, m, function( err, res, body ) {
+        var make = MakeAPI({ apiURL: hostNoAuth });
+
+        console.log("body", body);
+
+        make.url( m.make.url ).then( function( err, data ) {
+
+          console.log("data", data);
+
+          assert.ok( !err );
+          assert.ok( !!data );
+          assert.equal( data[ 0 ].url, m.make.url );
+          done();
+        });
+      });
+    });
+    
+  // for POSTed call parameters
+
+    it('should error if call parameters are erroneously present', function(done){
+      done();
+    });
+    
+    
+  // POSTed expected info
+
+    it ('should error if mimimum fields are not passed', function(done){
+      done();
+    });
+    
+    it('should create a new Make if minimum fields are passed', function (done){   
+      done();
+    });
+    
+    
+    it('should create a new Make if minimum fields + email are passed', function (done){
+      done();
+    });
+    
+    
+    it('should create a new Make if minimum fields + url are passed', function (done){
+      done();
+    });
+    
+    it('should create a new Make if minimum fields + url + email are passed', function (done){
+      done();
+    });
+    
+    
+    // for POSTed url
+    
+    it('should error if url is not present', function (done){
+
+      done();
+    });
+    
+    
+    it('should error if url is not a string', function (done){
+
+      done();
+    });
+    
+    
+    it('should error if url is not a valid URL', function (done){
+
+      done();
+    });
+
+    // for POSTed content type
+    
+    
+    it('should error if contentType is not present', function (done){
+
+      done();
+    });
+    
+    it('should error if contentType is not STRING', function (done){
+
+      done();
+    });
+    
+    it('should error if contentType is not recognized', function (done){
+
+      done();
+    });
+    
+    // for POSTed title 
+    
+    it('should error if title is not present', function (done){
+
+      done();
+    });
+    
+    it('should error if title is not a STRING', function (done){
+
+      done();
+    });
+    
+    it('should error if title contains a bad word', function (done){
+
+      done();
+    });
+
+    it( 'should error when title contains the illegal character', function( done ) {
+      done();
+    });
+
+    // for POSTed email
+   /* 
+    it('should error if email is not present', function (done){
+
+      var retVal = callServer({
+       url: "www.blah.com",
+       contentType: "text/plaintext",
+       title: "blah"
+     } );
+
+      assert.equal( retVal.hasOwnProperty(error, true )
+
+          done();
+    });
+    */
+    
+    it('should error if email is not STRING', function (done){
+
+      done();
+    });
+    
+    it('should error if email contains a bad word', function (done){
+
+      done();
+    });
+    
+    
+    it('should error if email format is not valid', function (done){
+
+      done();
+    });
+
+    // for return to POSTed data
+
+
+    it('should error if the return is not present', function (done){ 
+      done();
+    });
+    
+    it('should ... if the return is an error object', function (done){
+
+      done();
+    });
+    
+    it('should ... if the return is an object with the email property', function (done){
+
+      done();
+    });
+    
+    it('should ... if the return is an object with the title property', function (done){
+
+      done();
+    });
+    
+    // returned email
+    
+    it('should error if the return`s email property is NOT present', function (done){
+      done();
+    });
+    
+    it('should error if the return`s email property is NOT a STRING', function (done){
+      done();
+    });
+
+    it('should error if the return`s email property contains a bad word', function (done){
+      done();
+    });
+
+    it('should error if the return`s email property is of wrong format', function (done){
+      done();
+    });
+
+    it('should error if the return`s email property is too long (> ... chars)', function (done){
+      done();
+    });
+    
+    //returned title
+    
+    it('should error if the return`s title is not present', function (done){
+
+      done();
+    });
+    
+    it('should error if the the return`s title is not a STRING', function (done){
+      done();
+    });
+
+    it('should error if the return`s title contains a bad word', function (done){
+      done();
+    });
+
+    it('should error if the return`s title is too long (> ... chars)', function (done){
+      done();
+    });
+
+
+    it('should error if the return`s title is too short (< 1 chars)', function (done){ // ?
+      done();
+    });
+
+    it( 'should error if the return`s title contains illegal characters', function( done ) {
       done();
     });
   });
+  // for PUTed data
+  
+  describe('PUT /api/make/:id', function(){
 
-
-  it( 'make-api.js - id', function( done ) {
-    var m = unique();
-
-    apiHelper( 'post', api, 200, m, function( err, res, body ) {
-      var make = MakeAPI({ apiURL: hostNoAuth, auth: "travis:travis" });
-
-      console.log("body", body);
-
-      make.id( body.id ).then( function( err, data ) {
-
-        console.log("data", data);
-
-        assert.ok( !err );
-        assert.ok( !!data );
-        assert.equal( data[ 0 ].id, body.id );
-        done();
-      });
+    // call parameter is id:
+    
+    it('should update a Make if id has been passed', function(done){
+      done();
     });
+    
+    it('should error if id is not present', function(done){
+      done();
+    });
+    
+    it('should error if id format is wrong', function(done){
+      done();
+    });
+
+    // expected info is maker and make:
+    
+    it('should update a Make if maker and make have been passed', function(done){
+      done();
+    });
+    
+    it('should err if make and maker have not been passed', function(done){
+      done();
+    });
+    
+    // make`s conditions 
+    
+    it('should error if maker is not present', function(done){
+      done();
+    });
+    
+    it('should error if maker is not a STRING', function(done){
+      done();
+    });
+
+    it('should error if maker contains a bad word', function(done){
+      done();
+    });
+
+    it('should error if maker is too long (> ... chars)', function(done){
+      done();
+    });
+    
+    it('should error if maker is too short (< ... chars)', function(done){
+      done();
+    });
+    
+    it( 'should error if the return`s maker contains illegal characters', function( done ) {
+      done();
+    }); 
+
+    it('should update a Make if make is present', function(done){
+        done();
+    });
+      
+  }); // end of describe()
+  
+  // for data in DELETE req 
+  describe('DELETE /api/make/:id', function(){
+
+    // call parameter 
+
+    it('should delete a Make if id is present', function(done){
+      done();
+    }); 
+
+    
+    it('should error if id is not present', function(done){
+      done();
+    });
+
+
+    // expected info
+
+    it('should error if data is erroneously received', function(done){
+      done();
+    });
+
+    // return
+
+    it('should delete a Make if an error object has email and title:', function(done){
+      done();
+    });
+
+
+    // return's email
+    it('should error if email is not present', function (done){
+      done();
+    });
+
+    it('should error if email is not a STRING', function (done){
+      done();
+    });
+    
+
+    it('should error if email contains a bad word', function (done){
+      done();
+    });
+
+    it('should error if email format is not valid', function (done){
+      done();
+    });
+
+    it('should error if email is too long (> ... chars)', function (done){
+      done();
+    });
+    
+    // return's title
+
+    it('should error if title is not present', function (done){
+      done();
+    });
+
+    it('should error if title is not a STRING', function (done){
+      done();
+    });
+
+
+    it('should error if title contains a bad word', function (done){
+      done();
+    });
+
+    it('should error if title is too long (>... chars)', function (done){
+      done();
+    });
+
+    it('should error if title is too short (< ... chars)', function (done){
+      done();
+    });
+
+    it( 'should error if the return`s title contains illegal characters', function( done ) {
+       done();   
+    }); // end of illegalChars.forEach()
+
   });
 
 
-  it( 'make-api.js - url', function( done ) {
-    var m = unique();
 
-    apiHelper( 'post', api, 200, m, function( err, res, body ) {
-      var make = MakeAPI({ apiURL: hostNoAuth });
 
-      console.log("body", body);
-
-      make.url( m.make.url ).then( function( err, data ) {
-
-        console.log("data", data);
-
-        assert.ok( !err );
-        assert.ok( !!data );
-        assert.equal( data[ 0 ].url, m.make.url );
-        done();
-      });
-    });
-  });
-
-});
